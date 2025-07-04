d791b058ea95bb3957923a7b42878858d39c355c
falmuta96 <falmuta96@gmail.com>
2025-07-04 05:54:47 +0300

    Working playable condition

---
+++ Game.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    public class Gameplay([Optional] string passCode, [Optional] int maxAttempts)
+    {
+        public void Play()
+        {
+            Console.WriteLine("Can you break the code? Enter a valid guess.\n");
+
+            //string passCode = GenerateSecretCode();
+            int attempts = maxAttempts;
+
+            if (string.IsNullOrEmpty(passCode))
+            {
+                passCode = GenerateSecretCode();
+            }
+            else if (!IsValidGuess(passCode))
+            {
+                Console.WriteLine("Invalid Passcode \n");
+                return;
+            }
+
+            while (attempts > 0)
+            {
+                Console.Write($"Attempt {maxAttempts - attempts+1}/{maxAttempts}: " );
+                string guess = Console.ReadLine();
+
+                if (!IsValidGuess(guess))
+                {
+                    Console.WriteLine("Invalid guess. Enter 4 unique digits between 0 and 8.\n");
+                    continue;
+                }
+
+                if (guess == passCode)
+                {
+                    Console.WriteLine("Congratz! You did it!");
+                    return;
+                }
+
+                (int correctPos, int correctDigit) = EvaluateGuess(passCode, guess);
+                Console.WriteLine($"Well-placed pieces: {correctPos}\nMisplaced pieces: {correctDigit}\n");
+
+                attempts--;
+            }
+            Console.WriteLine($"Out of attempts! The pass code was: {passCode}");
+        }
+        private static string GenerateSecretCode()
+        {
+            Random rand = new Random();
+            List<int> digits = new List<int>();
+
+            while (digits.Count < 4)
+            {
+                int num = rand.Next(0,9); // digits from 0 to 8
+                if (!digits.Contains(num))
+                    digits.Add(num);
+            }
+
+            return string.Join("", digits);
+        }
+        static bool IsValidGuess(string guess)
+        {
+            if (guess.Length != 4) return false;
+
+            HashSet<char> digits = new HashSet<char>();
+
+            foreach (char c in guess)
+            {
+                if (!char.IsDigit(c) || c > '8') return false;
+                if (!digits.Add(c)) return false; // duplicate digit
+            }
+
+            return true;
+        }
+
+        private static (int, int) EvaluateGuess(string code, string guess)
+        {
+            int correctPos = 0;
+            int correctDigit = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (guess[i] == code[i])
+                {
+                    correctPos++;
+                }
+                else if (code.Contains(guess[i]))
+                {
+                    correctDigit++;
+                }
+            }
+
+            return (correctPos, correctDigit);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Mastermind;
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Help;
+using System.CommandLine.Parsing;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string passCode= "";
+        int maxAttempts;
+        Option<string> passCodeOption = new("-c")
+        {
+            Description = "The file to read and display on the console."
+        };
+        
+        Option<int> attemptOption = new("-t")
+        {
+            Description = "The file to read and display on the console.",
+            DefaultValueFactory = parseResult => 10
+        };
+       
+        RootCommand rootCommand = new("Sample app for System.CommandLine");
+        rootCommand.Options.Add(passCodeOption);
+        rootCommand.Options.Add(attemptOption);
+       
+        ParseResult parseResult = rootCommand.Parse(args);
+        //if(parseResult.GetValueForOption(HelpOption()))
+        if (parseResult.GetValue(passCodeOption) is string parsedCode)
+        {
+            passCode  = parsedCode;
+        }
+        if (parseResult.GetValue(attemptOption) is int parsedAttempts)
+        {
+            maxAttempts = parsedAttempts;
+        }
+        foreach (ParseError parseError in parseResult.Errors)
+        {
+            Console.Error.WriteLine(parseError.Message);
+        }
+        //rootCommand.Parse("-h").Invoke();
+        Gameplay gameplay = new Gameplay(passCode, maxAttempts);
+        gameplay.Play();
+    }
+}
